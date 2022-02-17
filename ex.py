96bd3e87c6e8f088ff44c36c4abf8ad83afd2e73
Sean Compton <seancompton@byui.edu>
2022-02-16 20:55:57 -0700

    I optimized the code to run faster

--- ex.py
+++ ex.py
@@ -10,8 +10,7 @@ def main():
     with open("ip_address.txt") as ip_address:
         ip_and_dns = get_dns(clean_up_data(ip_address))
         for i in ip_and_dns:
-            print(i[0], " ", i[1])
-            
+            print(i)     
 
 def clean_up_data(ip_address):
     address = []
@@ -22,13 +21,13 @@ def clean_up_data(ip_address):
 
 
 def get_dns(ip_address):
-    list_ip_dns = []
-    for ip in ip_address:
+    list_ip_dns = [None] * len(ip_address)
+    for i in range(len(ip_address)):
         try:
-            host = socket.gethostbyaddr(str(ip))
-            list_ip_dns.append([ip, host])
+            host = socket.gethostbyaddr(str(ip_address[i]))
+            list_ip_dns[i] = host
         except:
-            list_ip_dns.append([ip, "Sorry we had issues reaching this ip address please try pasting it into your browser"])
+            list_ip_dns[i] = f"Sorry we had issues reaching {ip_address[i]} IP address please try pasting it into your browser."
 
     return list_ip_dns
 
